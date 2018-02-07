11563b00e625b390d9f5293c8090f4baf53399eb
Tsukiyonocm <tsukiyonocm@yahoo.com>
2018-02-06 23:55:02 -0500

    reworked carousel, sticky nav add

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,16 @@
-// Variables
-var slideIndex = 0;
-var slides = document.getElementsByClassName("slides");
-var dots = document.getElementById("dots").children;
-
-showSlides();
-
-//Sets up the Slideshow
-function showSlides (){
-    displayReset();
-    //Slide Index used as input into showSlides
-    slideIndex++;
-    //Resets if higher then slides.length
-    if (slideIndex > slides.length){
-        slideIndex = 1;
-    }
-    dotsSlider();
-    //Sets the new slide to display block for viewing
-    slides[slideIndex-1].style.display = "block";
-    //Set active class to slides[slideIndex-1]
-    dots[slideIndex-1].classList.add("active");
-    //Time period till display reset
-    setTimeout(showSlides, 3000);
+// Sticky Navbar
+window.onscroll = function(){
+    stickyNav()
 }
 
-function displayReset (){
-    //Sets all to display: none on every iteration
-    for (i = 0; i < slides.length; i++){
-        slides[i].style.display = "none";
-        console.log(slideIndex);
-    }
-}
+var navbar = document.querySelector("nav");
+var sticky = navbar.offsetTop;
 
-function dotsSlider(){
-    //Activates dots per image slider
-    for (i = 0; i < dots.length; i++){
-        dots[i].className = dots[i].className.replace(" active", "");
+function stickyNav() {
+    if (window.pageYOffset >= sticky){
+        navbar.classList.add("stickyNav")
+    }
+    else {
+        navbar.classList.remove("stickyNav")
     }
 }
